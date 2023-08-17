3994e8a2a8811b8c17faa3315b5c7bf79e328e41
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-08-17 23:33:06 +0500

    api configured

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { OpenAI } from "langchain/llms/openai";
 import { SqlDatabase } from "langchain/sql_db";
 import { createSqlAgent, SqlToolkit } from "langchain/agents/toolkits/sql";
@@ -6,61 +7,62 @@ import { DataSource } from "typeorm";
 import { configDotenv } from "dotenv";
 
 const app = express();
+app.use(cors({ origin: "http://localhost:3000" }));
 
-app.get("/query", async (req, res) => {
+app.get("api/query", async (req, res) => {
   const prompt = req.query.prompt;
 
 console.log("prompt: " + prompt);
 
-  configDotenv();
+  // configDotenv();
 
-  const datasource = new DataSource({
-    type: "sqlite",
-    database: "./data/northwind.db",
-  });
+  // const datasource = new DataSource({
+  //   type: "sqlite",
+  //   database: "./data/northwind.db",
+  // });
 
-  const db = await SqlDatabase.fromDataSourceParams({
-    appDataSource: datasource,
-  });
+  // const db = await SqlDatabase.fromDataSourceParams({
+  //   appDataSource: datasource,
+  // });
 
-  const toolkit = new SqlToolkit(db);
-  const model = new OpenAI({
-    temperature: 0,
-  });
+  // const toolkit = new SqlToolkit(db);
+  // const model = new OpenAI({
+  //   temperature: 0,
+  // });
 
-  const executor = createSqlAgent(model, toolkit);
+  // const executor = createSqlAgent(model, toolkit);
 
-  let response = {
-    prompt: prompt,
-    output: "",
-    intermediateSteps: [],
-    error: "",
-  };
+  // let response = {
+  //   prompt: prompt,
+  //   output: "",
+  //   intermediateSteps: [],
+  //   error: "",
+  // };
 
-  try {
-    const result = await executor.call({ input: prompt });
+  // try {
+  //   const result = await executor.call({ input: prompt });
 
-    result.intermediateSteps.forEach((step) => {
-      if (step.action.tool === "query-sql") {
-        response.prompt = prompt;
-        response.output = step.observation;
-        response.intermediateSteps = result.intermediateSteps;
-      }
-    });
+  //   result.intermediateSteps.forEach((step) => {
+  //     if (step.action.tool === "query-sql") {
+  //       response.prompt = prompt;
+  //       response.output = step.observation;
+  //       response.intermediateSteps = result.intermediateSteps;
+  //     }
+  //   });
 
-    console.log(
-      `Intermediate steps ${JSON.stringify(result.intermediateSteps, null, 2)}`
-    );
+  //   console.log(
+  //     `Intermediate steps ${JSON.stringify(result.intermediateSteps, null, 2)}`
+  //   );
 
-    res.json(response);
-  } catch (e) {
-    console.log(e + " " + "my error message");
-    response.error = "Server error. Try again with a different prompt.";
+  //   res.json(response);
+  // } catch (e) {
+  //   console.log(e + " " + "my error message");
+  //   response.error = "Server error. Try again with a different prompt.";
 
-    res.status(500).json(response);
-  }
+  //   res.status(500).json(response);
+  // }
 
-  await datasource.destroy();
+  // await datasource.destroy();
 });
 
 app.listen(5000, () => {
