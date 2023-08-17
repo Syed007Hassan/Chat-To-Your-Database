d4a52a79894999e66ea20b5d635f5902db70d3ea
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-08-17 23:54:54 +0500

    api working

--- api/index.js
+++ api/index.js
@@ -14,57 +14,57 @@ app.get("/api/query", async (req, res) => {
 
 console.log("prompt: " + prompt);
 
-  res.json({ prompt: prompt, output: "output" });
+  // res.json({ prompt: prompt, output: "output" });
 
-  // configDotenv();
+  configDotenv();
 
-  // const datasource = new DataSource({
-  //   type: "sqlite",
-  //   database: "./data/northwind.db",
-  // });
+  const datasource = new DataSource({
+    type: "sqlite",
+    database: "./data/northwind.db",
+  });
 
-  // const db = await SqlDatabase.fromDataSourceParams({
-  //   appDataSource: datasource,
-  // });
+  const db = await SqlDatabase.fromDataSourceParams({
+    appDataSource: datasource,
+  });
 
-  // const toolkit = new SqlToolkit(db);
-  // const model = new OpenAI({
-  //   temperature: 0,
-  // });
+  const toolkit = new SqlToolkit(db);
+  const model = new OpenAI({
+    temperature: 0,
+  });
 
-  // const executor = createSqlAgent(model, toolkit);
+  const executor = createSqlAgent(model, toolkit);
 
-  // let response = {
-  //   prompt: prompt,
-  //   output: "",
-  //   intermediateSteps: [],
-  //   error: "",
-  // };
+  let response = {
+    prompt: prompt,
+    output: "",
+    intermediateSteps: [],
+    error: "",
+  };
 
-  // try {
-  //   const result = await executor.call({ input: prompt });
+  try {
+    const result = await executor.call({ input: prompt });
 
-  //   result.intermediateSteps.forEach((step) => {
-  //     if (step.action.tool === "query-sql") {
-  //       response.prompt = prompt;
-  //       response.output = step.observation;
-  //       response.intermediateSteps = result.intermediateSteps;
-  //     }
-  //   });
+    result.intermediateSteps.forEach((step) => {
+      if (step.action.tool === "query-sql") {
+        response.prompt = prompt;
+        response.output = step.observation;
+        response.intermediateSteps = result.intermediateSteps;
+      }
+    });
 
-  //   console.log(
-  //     `Intermediate steps ${JSON.stringify(result.intermediateSteps, null, 2)}`
-  //   );
+    console.log(
+      `Intermediate steps ${JSON.stringify(result.intermediateSteps, null, 2)}`
+    );
 
-  //   res.json(response);
-  // } catch (e) {
-  //   console.log(e + " " + "my error message");
-  //   response.error = "Server error. Try again with a different prompt.";
+    res.json(response);
+  } catch (e) {
+    console.log(e + " " + "my error message");
+    response.error = "Server error. Try again with a different prompt.";
 
-  //   res.status(500).json(response);
-  // }
+    res.status(500).json(response);
+  }
 
-  // await datasource.destroy();
+  await datasource.destroy();
 });
 
 app.listen(5000, () => {
