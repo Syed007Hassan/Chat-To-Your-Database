de8341b77b92b5b6b7895db2f2a10b5bbdaf1e7a
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-08-28 21:24:06 +0500

    orm connection setup

--- nest_api/src/app.module.ts
+++ nest_api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 // import { MongooseModule } from '@nestjs/mongoose';
 // import { AuthModule } from './auth/auth.module';
 // import { UserModule } from './user/user.module';
@@ -12,19 +13,26 @@ import { AiModule } from './ai/ai.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: `.env`,
     }),
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: './src/data/northwind.db',
-    // }),
     TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
-      database: "dvdrental",
-      port: 5432,
-      username: "postgres",
-      password: "fast",
-      schema: "public",
+      type: 'sqlite',
+      database: './src/data/northwind.db',
+    }),
+    // below is way to use async config service to get db name from .env file
+    TypeOrmModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get('DB_PORT'),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        schema: config.get('DB_SCHEMA'),
+        synchronize: true,
+        logging: true,
+      }),
+      inject: [ConfigService],
     }),
     AiModule,
     // MongooseModule.forRoot(process.env.MONGODB_URI),
