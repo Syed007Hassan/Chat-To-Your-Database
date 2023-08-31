31997f8739d783f277193b15b5ab7496aed8dd1b
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-08-31 20:19:17 +0500

    old intermediate steps removed

--- nest_api/src/ai/ai.controller.ts
+++ nest_api/src/ai/ai.controller.ts
@@ -4,19 +4,18 @@ import { AiResponse } from './dto/ai-response.dto';
 
 @Controller('ai')
 export class AiController {
+  constructor(private readonly aiService: AiService) {}
 
-    constructor(private readonly aiService: AiService){}
-
-    @Get('chat/:prompt')
-    async chat(@Param('prompt') prompt: string): Promise<{success:boolean, data?: AiResponse, message?: string}>{
-        try {
-            const aiResponse: AiResponse = await this.aiService.chat(prompt);
-            // console.log(aiResponse);
-            return {success: true, data: aiResponse};
-        }
-        catch(err){
-            return { success: false, message: err.message };
-        }
+  @Get('chat/:prompt')
+  async chat(
+    @Param('prompt') prompt: string,
+  ): Promise<{ success: boolean; data?: AiResponse; message?: string }> {
+    try {
+      const aiResponse: AiResponse = await this.aiService.chat(prompt);
+      // console.log(aiResponse);
+      return { success: true, data: aiResponse };
+    } catch (err) {
+      return { success: false, message: err.message };
     }
-
+  }
 }
--- nest_api/src/ai/ai.service.ts
+++ nest_api/src/ai/ai.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { OpenAI } from 'langchain';
-import { createSqlAgent, SqlToolkit } from "langchain/agents/toolkits/sql";
+import { OpenAI } from 'langchain/llms/openai';
+import { createSqlAgent, SqlToolkit } from 'langchain/agents/toolkits/sql';
 import { AiResponse } from './dto/ai-response.dto';
 import { SqlDatabase } from 'langchain/sql_db';
 import { DataSource } from 'typeorm';
@@ -9,80 +9,72 @@ import { SQL_SUFFIX, SQL_PREFIX } from './constants/prompt';
 
 @Injectable()
 export class AiService implements OnModuleInit {
-    private executor: any; 
-    private model: OpenAI; 
-    private toolkit: SqlToolkit;
+  private executor: any;
+  private model: OpenAI;
+  private toolkit: SqlToolkit;
 
-    constructor(private dataSource: DataSource) {}
+  constructor(private dataSource: DataSource) {}
 
-    async onModuleInit() {
-        const db = await SqlDatabase.fromDataSourceParams({
-            appDataSource: this.dataSource,
-        });
+  async onModuleInit() {
+    const db = await SqlDatabase.fromDataSourceParams({
+      appDataSource: this.dataSource,
+    });
 
-        this.model = new OpenAI({
-            openAIApiKey: process.env.OPENAI_API_KEY,
-            temperature: 0,
-        });
-        console.log('my model' + JSON.stringify(this.model));
+    this.model = new OpenAI({
+      openAIApiKey: process.env.OPENAI_API_KEY,
+      temperature: 0,
+    });
 
-        console.log(db);
+    this.toolkit = new SqlToolkit(db);
 
-        this.toolkit = new SqlToolkit(db);
+    this.executor = createSqlAgent(this.model, this.toolkit, {
+      topK: 10,
+      prefix: SQL_PREFIX,
+      suffix: SQL_SUFFIX,
+    });
+  }
 
-        this.executor = createSqlAgent(this.model, this.toolkit,{
-            topK: 10,
-            prefix: SQL_PREFIX,
-            suffix: SQL_SUFFIX,
-        });
+  async chat(prompt: string): Promise<AiResponse> {
+    let aiResponse: AiResponse = new AiResponse();
 
-    }
+    try {
+      const result = await this.executor.call({ input: prompt });
 
-    async chat(prompt: string): Promise<AiResponse>{
-
-        let aiResponse: AiResponse = new AiResponse();
-
-        try {
-            // const result = await this.executor.call({ input: prompt });
-        
-            // result.intermediateSteps.forEach((step) => {
-            //   if (step.action.tool === "query-sql") {
-            //     this.aiResponse.prompt = prompt;
-            //     this.aiResponse.sqlQuery = step.action.toolInput;
-            //     this.aiResponse.result = JSON.parse(step.observation);
-            //   }
-            // });
-        
-            const result = RESULT;
-            console.log("Hero"+aiResponse);
-            result.intermediateSteps.forEach((step) => {
-              if (step.action.tool === "query-sql") {
-                aiResponse.prompt = prompt;
-                aiResponse.sqlQuery = step.action.toolInput;
-                aiResponse.sqlQuery = aiResponse.sqlQuery.replace(/\\/g, '').replace(/"/g, '');              
-                try {
-                    console.log(step.observation)
-                    const observation = JSON.parse(step.observation);
-                    if (Array.isArray(observation) && observation.every(obj => typeof obj === 'object')) {
-                        aiResponse.result = observation;
-                    }
-                  } catch (error) {
-                    console.log(error);
-                  }
-            
+      result.intermediateSteps.forEach((step) => {
+        if (step.action.tool === 'query-sql') {
+          aiResponse.prompt = prompt;
+          aiResponse.sqlQuery = step.action.toolInput;
+          aiResponse.sqlQuery = aiResponse.sqlQuery
+            .replace(/\\/g, '')
+            .replace(/"/g, '');
+          try {
+            const observation = JSON.parse(step.observation);
+            if (
+              Array.isArray(observation) &&
+              observation.every((obj) => typeof obj === 'object')
+            ) {
+              aiResponse.result = observation;
             }
-            });
-
-            // console.log(
-            //   `Intermediate steps ${JSON.stringify(result.intermediateSteps, null, 2)}`
-            // );
-        
-            return aiResponse;
-          } catch (e) {
-            console.log(e + " " + "my error message");
-            aiResponse.error = "Server error. Try again with a different prompt.";
-        
-            return aiResponse;
+          } catch (error) {
+            console.log(error);
           }
+        }
+      });
+
+      console.log(
+        `Intermediate steps ${JSON.stringify(
+          result.intermediateSteps,
+          null,
+          2,
+        )}`,
+      );
+
+      return aiResponse;
+    } catch (e) {
+      console.log(e + ' ' + 'my error message');
+      aiResponse.error = 'Server error. Try again with a different prompt.';
+
+      return aiResponse;
     }
+  }
 }
