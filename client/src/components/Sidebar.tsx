6ec498a5ff4a762d5960d43145ca640a0b173c83
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-09-16 22:08:02 +0500

    queryHistory added in sidebar

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 export interface QueryHistory {
   id: string;
@@ -11,45 +12,56 @@ export interface QueryHistories {
   queryHistories: QueryHistory[];
 }
 
-const Sidebar = ({ queryHistories }: QueryHistories) => {
+const Sidebar = () => {
   const [selectedQueryHistory, setSelectedQueryHistory] =
     useState<QueryHistory | null>(null);
+  const [chatHistory, setChatHistory] = useState<QueryHistory[]>([]);
+
+  useEffect(() => {
+    const fetchChatHistory = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/ai/getAllChatHistory"
+        );
+        setChatHistory(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchChatHistory();
+  }, []);
 
   useEffect(() => {
     // Do something when the selected query history changes
   }, [selectedQueryHistory]);
 
   return (
-    <>
-      <aside
-        id="logo-sidebar"
-        className="fixed top-0 left-0 overflow-x-hidden z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm-md:translate-x-0 sm-md:flex-shrink-0 sm-md:static dark:bg-slate-900 dark:border-gray-700"
-        aria-label="Sidebar"
-      >
-        <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-slate-900">
-          <ul className="space-y-2 font-medium">
-            <li>
+    <aside
+      id="logo-sidebar"
+      className="fixed top-0 left-0 overflow-x-hidden z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm-md:translate-x-0 sm-md:flex-shrink-0 sm-md:static dark:bg-slate-900 dark:border-gray-700"
+      aria-label="Sidebar"
+    >
+      <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-slate-900">
+        <ul className="space-y-2 font-medium">
+          {chatHistory.map((history) => (
+            <li key={history.id}>
               <a
                 href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                className={`flex items-center p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 ${
+                  selectedQueryHistory?.id === history.id
+                    ? "bg-gray-100 dark:bg-gray-700"
+                    : ""
+                }`}
+                onClick={() => setSelectedQueryHistory(history)}
               >
-                <svg
-                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 21"
-                >
-                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-                </svg>
-                <span className="ml-3">History</span>
+                <span className="ml-3">{history.prompt}</span>
               </a>
             </li>
-          </ul>
-        </div>
-      </aside>
-    </>
+          ))}
+        </ul>
+      </div>
+    </aside>
   );
 };
 
